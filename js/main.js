b8a7200c2470a22ca6a11070b942af55b171e5a4
Ericdid <ericdidom@gmail.com>
2023-09-11 03:15:10 +0200

    add.messageBox-Working

--- js/main.js
+++ js/main.js
@@ -5,12 +5,23 @@ createApp({
     return {
       contacts,
       selectedUser: 0,
+
+      newMessage: {
+        date: "empty",
+        message: "",
+        status: "sent",
+      },
     };
   },
 
   methods: {
     newSelectedUser(index) {
       this.selectedUser = index;
+    },
+
+    sendMessage() {
+      staticMessage = { ...this.newMessage };
+      this.contacts[this.selectedUser].messages.push(staticMessage);
     },
   },
 }).mount("#app");
